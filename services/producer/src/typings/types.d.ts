2ccfbdb0fff97e23e2fad186d883cecabe03ac6d
amr.metwally <amr.metwally@logarithmo.de>
2021-04-04 18:43:24 +0200

    rm some types

--- services/producer/src/typings/types.d.ts
+++ services/producer/src/typings/types.d.ts
@@ -5,14 +5,9 @@ declare global {
     interface ProcessEnv {
       PRODUCER_PORT_CONTAINER?: string;
       PRODUCER_PORT_HOST?: string;
-      CONSUMER_PORT_CONTAINER?: string;
-      CONSUMER_PORT_HOST?: string;
       KAFKA_PORT_CONTAINER?: string;
-      KAFKA_PORT_HOST?: string;
       PRODUCER_CONTAINER_NAME?: string;
-      CONSUMER_CONTAINER_NAME?: string;
       KAFKA_CONTAINER_NAME?: string;
-      TOPICS?: string;
     }
   }
 }
