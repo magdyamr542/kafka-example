dbc3778dfa530ba0b0a65d79c03c7c203d7fed8c
amr.metwally <amr.metwally@logarithmo.de>
2021-04-10 12:55:12 +0200

    added logging json messages for the response

--- services/producer/src/index.ts
+++ services/producer/src/index.ts
@@ -5,21 +5,37 @@ import { router } from "./routes/routes";
 import { logger } from "./utils/winston";
 import { NextFunction, Request, Response } from "express";
 
-export const loggingMiddleware = (
-  req: Request,
+export const resLoggingMiddleware = (
+  _: Request,
   res: Response,
   next: NextFunction
 ) => {
-  // logging
-  logger.info(`URL: ${req.originalUrl}`);
-  logger.info(`IP: ${getRequestIp(req)}`);
+  const originalFunc = res.json;
+  res.json = function (this: typeof res.json, obj: any) {
+    logger.info("Got Response is: ");
+    logger.info(obj);
+    originalFunc.call(this, obj);
+  } as typeof originalFunc;
   next();
 };
+
+export const reqLoggingMiddleware = (
+  req: Request,
+  _: Response,
+  next: NextFunction
+) => {
+  // logging
+  logger.info(`Got Request URL: ${req.originalUrl}`);
+  logger.info(`Got Request IP: ${getRequestIp(req)}`);
+  next();
+};
+
 const env = getEnv();
 const main = async () => {
   const app = express();
   app.use(cors());
-  app.use(loggingMiddleware);
+  app.use(reqLoggingMiddleware);
+  app.use(resLoggingMiddleware);
   app.use(express.json());
   app.use("/", router);
   app.listen(env.PRODUCER_PORT_CONTAINER, () =>
